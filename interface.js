56d27c56252ddda425803e9fd098bbbd4bd85bca
albiebabie <albiebabie@gmail.com>
2016-08-05 10:52:02 +0100

    refactored displaying chosen city temp

--- interface.js
+++ interface.js
@@ -38,12 +38,20 @@ $(document).ready(function(){
     $('#current-temperature').text(data.main.temp);
   });
 
+  displayWeather('London');
+
   $('#select-city').submit(function(event){
     event.preventDefault();
     var city = $('#current-city').val();
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + ',&appid=6b9d318fe42ee8c5f85ba016a32d5492&units=metric', function(data){
+    displayWeather(city)
+  });
+
+  function displayWeather(city){
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = ',&appid=6b9d318fe42ee8c5f85ba016a32d5492';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data){
       $('#current-temperature').text(data.main.temp);
     });
-  });
-  
+  };
 });
